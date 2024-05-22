1102b768fe993da6eaead65772ddf4d4dc058020
dhfkdlsj <105478203+dhfkdlsj@users.noreply.github.com>
2024-05-22 23:12:52 +0900

    [Bronze II] Title: 진법 변환, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.py
+++ 백준/Bronze/2745. 진법 변환/진법 변환.py
@@ -2,10 +2,4 @@ N, B = map(str,input().split())
 num = 0
 B = int(B)
 
-for i in range(len(N)):
-    if ord(N[i]) > 57:
-        num += (ord(N[i])-55)*(B**(len(N)-i-1))
-    else:
-        num += (int(N[i]))*(B**(len(N)-i-1))
-
-print(num)
+print(int(N,B))
