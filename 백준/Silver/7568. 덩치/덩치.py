98795d9f84e8158c981ad1c937bee971b45e979c
dhfkdlsj <105478203+dhfkdlsj@users.noreply.github.com>
2024-06-16 15:44:10 +0900

    [Silver V] Title: 덩치, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.py
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -1,13 +1,12 @@
-num_student = int(input())
-student_list = []
+import sys
+input = sys.stdin.readline
 
-for _ in range(num_student):
-    weight, height = map(int, input().split())
-    student_list.append((weight, height))
+N = int(input())
+S = [list(map(int,input().split())) for _ in range(N)]
 
-for i in student_list:
-    rank = 1
-    for j in student_list:
-        if i[0] < j[0] and i[1] < j[1]:
-                rank += 1
-    print(rank, end = " ")
+for i in S:
+  count = 1
+  for j in S:
+    if i[0] < j[0] and i[1] < j[1]:
+      count += 1
+  print(count, end= " ")
