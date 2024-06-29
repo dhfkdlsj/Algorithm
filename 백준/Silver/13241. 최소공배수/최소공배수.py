0a4ddd19ffc09cd38b049eea570e1cb24b295812
dhfkdlsj <105478203+dhfkdlsj@users.noreply.github.com>
2024-06-29 14:59:52 +0900

    [Silver V] Title: 최소공배수, Time: 36 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Silver/13241. 최소공배수/최소공배수.py
+++ 백준/Silver/13241. 최소공배수/최소공배수.py
@@ -1,12 +1,5 @@
 import sys
 import math
-
 input = sys.stdin.readline
-
-answer = ''
-
 A, B = map(int, input().split())
-gcd = math.gcd(A, B)
-lcm = (A * B) // gcd
-
-print(lcm)
+print((A * B)//(math.gcd(A, B)))
