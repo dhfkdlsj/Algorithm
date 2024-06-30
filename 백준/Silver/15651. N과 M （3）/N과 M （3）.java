b70f527c5b5cce2fa3cc3bddfb0c43962368883e
dhfkdlsj <105478203+dhfkdlsj@users.noreply.github.com>
2024-06-30 12:47:54 +0900

    [Silver III] Title: N과 M (3), Time: 396 ms, Memory: 68708 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N,R;
+    static int[]a, b;
+    static StringBuilder sb = new StringBuilder();
+    static void nm3(int cnt){
+        if(cnt == R){
+            for(int x:b) sb.append(x).append(" ");
+            sb.append("\n");
+            return;
+        }
+        for(int i=0; i<N; i++){
+            b[cnt] = a[i];
+            nm3(cnt+1);
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+
+        N = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+
+        a = new int[N];
+        b = new int[R];
+
+        for(int i=0; i<N; i++){
+            a[i] = i+1;
+        }
+
+        nm3(0);
+        System.out.println(sb);
+
+    }
+}
