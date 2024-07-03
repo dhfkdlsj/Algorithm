cd62ce218c53804bea99c73d6e4012598b98016c
dhfkdlsj <105478203+dhfkdlsj@users.noreply.github.com>
2024-07-03 11:35:08 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 1584 ms, Memory: 252368 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -2,50 +2,50 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int N;
-	static int[][] g;
-	static int[] p;
-	
-	static void make() {
-		p =new int[N];
-		for(int i=1; i<N; i++) p[i] = i;
-	}
-	static int find(int a) {
-		if(p[a]==a) return a;
-		return find(p[a]);
-	}
-	static boolean union(int a, int b) {
-		int aRoot = find(a);
-		int bRoot = find(b);
-		if(aRoot==bRoot) return false;
-		p[bRoot] = aRoot;
-		return true;
-	}
-	
-	public static void main(String[] args) throws Exception{
-		Scanner sc = new Scanner(System.in);
-		N=sc.nextInt()+1;
-		int E=sc.nextInt();
-		g = new int[E][3];
-		
-		for(int i=0; i<E; i++) {
-			int from = sc.nextInt();
-			int to = sc.nextInt();
-			int cost = sc.nextInt();
-			g[i] = new int[] {from,to,cost};
-		}
-		Arrays.sort(g,(o1,o2)->Integer.compare(o1[2], o2[2]));
-		make();
-		int sum =0, cnt=0;
-		for(int[]e:g) {
-			if(union(e[0],e[1])) {
-				sum+= e[2];
-				if(++cnt==N-1) break;
-			}
-		}
-		System.out.println(sum);
-		
-		sc.close();
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt(); // 정점의 수
+        int M = sc.nextInt(); // 간선의 수
+        List<int[]>[] g = new List[N + 1];
+        for (int i = 1; i < N + 1; i++) {
+            g[i] = new ArrayList<>();
+        }
 
+        for (int i = 0; i < M; i++) { // 간선의 수 만큼 입력 받음
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int c = sc.nextInt();
+            g[x].add(new int[] { y, c });
+            g[y].add(new int[] { x, c });
+        }
+
+        boolean[] v = new boolean[N + 1];
+        int[] w = new int[N + 1]; // 프림 알고리즘을 위한 가중치 배열
+        Arrays.fill(w, Integer.MAX_VALUE);
+
+        int sum = 0;
+        w[1] = 0;
+        for (int i = 0; i < N; i++) {
+            int min = Integer.MAX_VALUE;
+            int minVertex = -1;
+            for (int j = 1; j <= N; j++) {
+                if (!v[j] && min > w[j]) {
+                    min = w[j];
+                    minVertex = j;
+                }
+            }
+            if (minVertex == -1)
+                break;
+            v[minVertex] = true;
+            sum += min;
+            for (int[] j : g[minVertex]) {
+                if (!v[j[0]] && w[j[0]] > j[1]) {
+                    w[j[0]] = j[1];
+                }
+            }
+        }
+        System.out.println(sum);
+
+        sc.close();
+    }
 }
