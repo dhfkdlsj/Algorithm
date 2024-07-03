9e945d0ab76201d11da64cf2c8d3c56015c67f72
dhfkdlsj <105478203+dhfkdlsj@users.noreply.github.com>
2024-07-03 16:44:01 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 1260 ms, Memory: 253080 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -2,48 +2,52 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int M = sc.nextInt();
-		List<int[]>[] g = new List[N+1];
-		for (int i = 1; i < N+1; i++) {
-			g[i] = new ArrayList<>();
-		}
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt(); // Number of nodes
+        int M = sc.nextInt(); // Number of edges
+        List<int[]>[] g = new List[N+1];
+        for (int i = 1; i <= N; i++) {
+            g[i] = new ArrayList<>();
+        }
 
-		for (int i = 0; i < M; i++) {
-			int x = sc.nextInt();
-			int y = sc.nextInt();
-			int c = sc.nextInt();
-			g[x].add(new int[] { y, c });
-			g[y].add(new int[] { x, c });
-		}
+        for (int i = 0; i < M; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int c = sc.nextInt();
+            g[x].add(new int[] { y, c });
+            g[y].add(new int[] { x, c });
+        }
 
-		boolean[] v = new boolean[N+1];
-		int[] w = new int[N+1]; // 프림w[]
-		Arrays.fill(w, Integer.MAX_VALUE);
+        boolean[] v = new boolean[N+1];
+        int[] w = new int[N+1]; // Weight array for Prim's algorithm
+        Arrays.fill(w, Integer.MAX_VALUE);
 
-		int sum = 0;
-		w[1] = 0;
-		for (int i = 0; i < N; i++) {
-			int min = Integer.MAX_VALUE;
-			int minVertex = -1;
-			for (int j = 1; j <= N; j++) {
-				if (!v[j] && min > w[j]) {
-					min = w[j];
-					minVertex = j;
-				}
-			}
-			v[minVertex] = true;
-			sum += min;
-			for (int[] j : g[minVertex]) {
-				if (!v[j[0]] && w[j[0]] > j[1]) {
-					w[j[0]] = j[1];
-				}
-			}
-		}
-		System.out.println(sum);
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[1], o2[1]));
+        int sum = 0, cnt = 0;
+        w[1] = 0; // Start from node 1
+        pq.offer(new int[] {1, 0});
 
-		sc.close();
-	}
+        while (!pq.isEmpty()) {
+            int[] vw = pq.poll();
+            int minVertex = vw[0];
+            int min = vw[1];
+            if (v[minVertex]) continue;
+
+            v[minVertex] = true;
+            sum += min;
+
+            if (cnt++ == N-1) break;
+
+            for (int[] j : g[minVertex]) {
+            	 if(!v[j[0]] && w[j[0]]>j[1]) {
+        			 w[j[0]] = j[1];
+        			 pq.offer(j);  
+        		 }
+        	 }
+        }
+        System.out.println(sum);
+        
+        sc.close();
+    }
 }
