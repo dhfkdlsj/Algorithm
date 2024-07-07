61b3d9256475cb84bf4ca6cd3c9c2e57620fe796
dhfkdlsj <105478203+dhfkdlsj@users.noreply.github.com>
2024-07-07 13:17:09 +0900

    [Silver II] Title: 부분수열의 합, Time: 108 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -3,21 +3,21 @@ import java.util.*;
 
 public class Main {
     static int N, S, count;
-    static int[] A, b;
+    static int[] A;
 
-    static void sum(int cnt, int start, int R) {
-        if (cnt == R) {
-            int total = 0;
-            for (int i = 0; i < R; i++) {
-                total += b[i];
-            }
-            if (total == S) count++;
+    static void findSubsets(int index, int currentSum) {
+        // index가 N에 도달하면 종료
+        if (index == N) {
+            // currentSum이 S와 같고 부분집합이 공집합이 아닌 경우 count 증가
+            if (currentSum == S) count++;
             return;
         }
-        for (int i = start; i < N; i++) {
-            b[cnt] = A[i];
-            sum(cnt + 1, i+1, R);
-        }
+
+        // 현재 원소를 포함하지 않는 경우
+        findSubsets(index + 1, currentSum);
+
+        // 현재 원소를 포함하는 경우
+        findSubsets(index + 1, currentSum + A[index]);
     }
 
     public static void main(String[] args) throws Exception {
@@ -27,15 +27,16 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         S = Integer.parseInt(st.nextToken());
         A = new int[N];
-        b = new int[N];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             A[i] = Integer.parseInt(st.nextToken());
         }
         count = 0;
-        for (int i = 1; i < N + 1; i++) {
-            sum(0, 0, i);
-        }
+        findSubsets(0, 0);
+
+        // S가 0일 경우 공집합을 제외해야 하므로 count를 하나 줄여야 함
+        if (S == 0) count--;
+
         System.out.println(count);
     }
 }
