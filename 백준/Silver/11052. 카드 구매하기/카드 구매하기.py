1569ba08fa324ede2614ce5779aece392c1443d8
dhfkdlsj <105478203+dhfkdlsj@users.noreply.github.com>
2024-07-20 20:50:51 +0900

    [Silver I] Title: 카드 구매하기, Time: 216 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -7,9 +7,6 @@ P = [0 for _ in range(n+1)]
 
 for i in range(1,n+1):
     for j in range(i,n+1):
-        if(j%i==0):
-            P[j] = max(P[j],S[i-1]*(j//i))
-        elif j>i:
-            P[j] = max(P[j],S[i-1]+P[j-i])
+         P[j] = max(P[j],S[i-1]+P[j-i])
 
 print(P[n])
