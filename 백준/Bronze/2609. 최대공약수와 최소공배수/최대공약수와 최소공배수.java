f40e3b21584ef162cb11c7c13dc7f893b5695995
dhfkdlsj <105478203+dhfkdlsj@users.noreply.github.com>
2024-07-29 10:35:34 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 112 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int gcd = gcd(N, M);
+        int lcm = (N / gcd) * M;
+
+        System.out.println(gcd);
+        System.out.println(lcm);
+    }
+
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
