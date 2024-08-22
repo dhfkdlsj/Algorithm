2853857f71d1edd3638628b408d7b39a433bcb69
dhfkdlsj <105478203+dhfkdlsj@users.noreply.github.com>
2024-08-22 13:29:58 +0900

    [Gold V] Title: 동전, Time: 60 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/9084. 동전/동전.py
+++ 백준/Gold/9084. 동전/동전.py
@@ -8,16 +8,12 @@ for _ in range(t):
     coins = list(map(int, input().split()))
     m = int(input())
 
-    # memoization을 위한 리스트 선언
     d = [0] * (m + 1)
     d[0] = 1
 
 
     for coin in coins:
-        for i in range(m + 1):
-            # a_(i-k) 를 만드는 방법이 존재한다면 
-            # 이전 경우의 수에 현재 동전으로 만들 수 있는 경우의 수를 더한다.
-            if i >= coin:
-                d[i] += d[i - coin]
+        for i in range(coin,m + 1):
+            d[i] += d[i - coin]
 
     print(d[m])
