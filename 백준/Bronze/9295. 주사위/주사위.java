ebc34417d7aba78996858173687dd193b6a9219a
dhfkdlsj <105478203+dhfkdlsj@users.noreply.github.com>
2024-10-24 09:14:30 +0900

    [Bronze IV] Title: 주사위, Time: 176 ms, Memory: 16392 KB -BaekjoonHub

---
+++ 백준/Bronze/9295. 주사위/주사위.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int i = 1; i <= T; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
+			
+			System.out.println("Case " + i + ": " + (n + m));
+		}
+	}
+
+}
