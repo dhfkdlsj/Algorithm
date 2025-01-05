23e8e0ac39cf4a71f7a77e03e3739501fc8819b0
dhfkdlsj <105478203+dhfkdlsj@users.noreply.github.com>
2025-01-05 16:34:46 +0900

    [Silver III] Title: N과 M (2), Time: 104 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,35 +1,43 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
-	
-	static void comb(int cnt,int start,int N, int R, int[] a, int[] b){
-		if(cnt==R){
-			for (int i = 0; i < b.length; i++) {
-				System.out.print(b[i]+" ");
-			}
-			System.out.println();
-			return;
-		}
-		for(int i=start; i<N; i++){
-			//if(v[i]) continue;//
-			//v[i]=true;//
-			b[cnt]=a[i];
-			comb(cnt+1,i+1,N,R,a,b); //4C3
-			//comb(cnt+1,i); -> 20개 4H3 중복 포함
-			//v[i]=false;//
-		}
-	}
-	
-	public static void main(String[] args) throws Exception{
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int R = sc.nextInt();
-		int[] a=new int[N], b=new int[R];
-		for(int i = 0; i < N; i++) {
-			a[i] = i+1;
-		}
-		comb(0,0,N,R,a,b);
-		sc.close();
-	}
+public class Main {
+    static int N;
+    static int M;
+    static int[] numbers;
+    static int[] selected;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        numbers = new int[N];
+        for (int i = 0; i < N; i++) {
+            numbers[i] = i + 1;
+        }
+        selected = new int[M];
+
+        sequence(0, 0);
+
+        System.out.println(sb);
+        br.close();
+    }
+
+    private static void sequence(int start, int cnt) {
+        if (cnt == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(selected[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = start; i < N; i++) {
+            selected[cnt] = numbers[i];
+            sequence(i + 1, cnt + 1);
+        }
+    }
 }
