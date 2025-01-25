3db759fcc3a28f03604afd96dec246016e3d28ca
dhfkdlsj <105478203+dhfkdlsj@users.noreply.github.com>
2025-01-25 10:12:08 +0900

    [Silver III] Title: 바이러스, Time: 104 ms, Memory: 14060 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,38 +1,45 @@
 import java.util.*;
 import java.io.*;
-
 public class Main {
-	static int Computer,Pair,cnt;
-	static ArrayList<Integer>[] list;
-	static boolean[] v;
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		Computer = Integer.parseInt(br.readLine());
-		Pair = Integer.parseInt(br.readLine());
-		v = new boolean[Computer+1];
-		list = new ArrayList[Computer+1];
-		
-		for(int i =0; i< Computer+1; i++) {
-			list[i] = new ArrayList<>();
-		}
-		for(int i =0; i< Pair; i++) {
-			st = new StringTokenizer(br.readLine());
-			int from = Integer.parseInt(st.nextToken());
-			int to = Integer.parseInt(st.nextToken());
-			list[from].add(to);
-			list[to].add(from);
-		}
-		dfs(1);
-		System.out.println(cnt);
-	}
-	static void dfs(int i) {
-		v[i] = true;
-		for(int j=0; j<list[i].size(); j++) {
-			if(!v[list[i].get(j)]) {
-				cnt++;
-				dfs(list[i].get(j));
-			}
-		}
-	}
+    static int computer,pair,count;
+    static boolean[] v;
+    static ArrayList<Integer>[] list;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        computer = Integer.parseInt(br.readLine());
+        pair = Integer.parseInt(br.readLine());
+        list = new ArrayList[computer+1];
+        v = new boolean[computer+1];
+
+        for (int i = 1; i < computer+1; i++) {
+            list[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < pair; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            list[from].add(to);
+            list[to].add(from);
+        }
+        count = 0;
+        dfs(1);
+        System.out.println(count);
+
+        br.close();
+    }
+
+    private static void dfs(int number) {
+        v[number] = true;
+        for (int i : list[number]) {
+            if (!v[i]) {
+                count++;
+                dfs(i);
+            }
+        }
+    }
+
+
 }
