3dac439ad2be9585d5dbcd5bf0c95de0c9854af2
dhfkdlsj <105478203+dhfkdlsj@users.noreply.github.com>
2025-02-15 19:15:35 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1084 ms, Memory: 83340 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,44 +1,52 @@
 import java.io.*;
 import java.util.*;
+
 public class Main {
-	static int N,a,b;
-	static boolean[] v;
-	static ArrayList<Integer>[] list;
-	static int[] x;
-	
-	static void tree(int i) {
-		v[i] = true;
-		for( int j = 0; j < list[i].size(); j++) {
-			if(!v[list[i].get(j)]) {
-				v[list[i].get(j)] = true;
-				x[list[i].get(j)] = i;
-				tree(list[i].get(j));
-			}
-		}
-	}
-	
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		x = new int[N+1];
-		
-		list = new ArrayList[N+1];
-		v = new boolean[N+1];
-		for(int i = 1; i <=N; i++) {
-			list[i] = new ArrayList<Integer>();
-		}
-			
-		for(int i = 1; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			a = Integer.parseInt(st.nextToken());
-			b = Integer.parseInt(st.nextToken());
-			list[a].add(b);
-			list[b].add(a);
-		}
-		tree(1);
-		for(int i = 2; i<x.length; i++) {
-			System.out.println(x[i]);
-		}
-	}
+    static int nodeCount, node1, node2;
+    static boolean[] visited;
+    static ArrayList<Integer>[] adjList;
+    static int[] parent;
+    
+    static void findParents(int currentNode) {
+        visited[currentNode] = true;
+        // 현재 노드와 연결된 모든 노드 탐색
+        for (int i = 0; i < adjList[currentNode].size(); i++) {
+            int nextNode = adjList[currentNode].get(i);
+            if (!visited[nextNode]) {  // 방문하지 않았다면
+                visited[nextNode] = true;
+                parent[nextNode] = currentNode;  // 부모 저장
+                findParents(nextNode);
+            }
+        }
+    }
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        nodeCount = Integer.parseInt(st.nextToken());
+        
+        parent = new int[nodeCount + 1];
+        visited = new boolean[nodeCount + 1];
+        adjList = new ArrayList[nodeCount + 1];
+        
+        for (int i = 1; i <= nodeCount; i++) {
+            adjList[i] = new ArrayList<>();
+        }
+        
+        // 트리의 간선 정보 입력
+        for (int i = 1; i < nodeCount; i++) {
+            st = new StringTokenizer(br.readLine());
+            node1 = Integer.parseInt(st.nextToken());
+            node2 = Integer.parseInt(st.nextToken());
+            adjList[node1].add(node2);
+            adjList[node2].add(node1);
+        }
+        
+        findParents(1); // 루트 노드부터 DFS 실행
+        
+        // 2번 노드부터 부모 노드 출력
+        for (int i = 2; i <= nodeCount; i++) {
+            System.out.println(parent[i]);
+        }
+    }
 }
